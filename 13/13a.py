196073b93ef879ca061110cfb9370452bcb457c8
peter.gergel <peter.gergel@segron.com>
2019-12-13 09:30:12 +0100

    13

---
+++ 13/13a.py
@@ -0,0 +1,165 @@
+program = list(map(int, open('13.txt').read().split(',')))
+program.extend([0] * 10000)
+input_value = 2
+
+outputs = []
+
+def interpret_opcode(opcode):
+    instruction = opcode % 100
+    parameters = opcode // 100
+
+    parameter_mode = [0, 0, 0]
+    for idx in range(3):
+        parameter_mode[idx] = (parameters % 10)
+        parameters = parameters // 10
+
+    return instruction, parameter_mode
+
+ip = 0
+rb = 0
+while program[ip] != 99:
+    opcode = program[ip]
+    instruction, parameter_mode = interpret_opcode(opcode)
+
+    if instruction == 1:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        result = first_val + second_val
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        program[output_address] = result
+        ip += 4
+    elif instruction == 2:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        result = first_val * second_val
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        program[output_address] = result
+        ip += 4
+    elif instruction == 3:
+        input_address = program[ip+1]
+        if parameter_mode[0] == 2:
+            input_address += rb
+        program[input_address] = input_value
+        ip += 2
+    elif instruction == 4:
+        output_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            output_val = program[output_val]
+        elif parameter_mode[0] == 2:
+            output_val = program[output_val + rb]
+        outputs.append(output_val)
+        ip += 2
+    elif instruction == 5:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        if first_val != 0:
+            ip = second_val
+        else:
+            ip += 3
+    elif instruction == 6:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        if first_val == 0:
+            ip = second_val
+        else:
+            ip += 3
+    elif instruction == 7:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        if first_val < second_val:
+            program[output_address] = 1
+        else:
+            program[output_address] = 0
+        ip += 4
+    elif instruction == 8:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        if first_val == second_val:
+            program[output_address] = 1
+        else:
+            program[output_address] = 0
+        ip += 4
+    elif instruction == 9:
+        val = program[ip+1]
+        if parameter_mode[0] == 0:
+            val = program[val]
+        elif parameter_mode[0] == 2:
+            val = program[val + rb]
+        rb += val
+        ip += 2
+
+print(outputs[2::3].count(2))
---
+++ 13/13b.py
@@ -0,0 +1,206 @@
+program = list(map(int, open('13.txt').read().split(',')))
+program.extend([0] * 10000)
+program[0] = 2
+
+width = 38  # from part A
+height = 22  # from part A
+world = [[0 for _ in range(height)] for _ in range(width)]
+outputs = []
+
+tileId_to_char = {
+    0: ' ',  # empty
+    1: '#',  # wall
+    2: 'B',  # block
+    3: 'H',  # horizontal paddle
+    4: 'O',  # ball
+}
+
+paddle_x, paddle_y = -1, -1
+ball_x, ball_y = -1, -1
+score = 0
+
+def print_world():
+    for row in world:
+        for tileId in row:
+            print(tileId_to_char[tileId], end='')
+        print()
+
+def interpret_opcode(opcode):
+    instruction = opcode % 100
+    parameters = opcode // 100
+
+    parameter_mode = [0, 0, 0]
+    for idx in range(3):
+        parameter_mode[idx] = (parameters % 10)
+        parameters = parameters // 10
+
+    return instruction, parameter_mode
+
+ip = 0
+rb = 0
+while program[ip] != 99:
+    opcode = program[ip]
+    instruction, parameter_mode = interpret_opcode(opcode)
+
+    if instruction == 1:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        result = first_val + second_val
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        program[output_address] = result
+        ip += 4
+    elif instruction == 2:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        result = first_val * second_val
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        program[output_address] = result
+        ip += 4
+    elif instruction == 3:
+        input_address = program[ip+1]
+        if parameter_mode[0] == 2:
+            input_address += rb
+        if paddle_x > ball_x:
+            input_value = -1
+        elif paddle_x == ball_x:
+            input_value = 0
+        else:
+            input_value = 1
+        program[input_address] = input_value
+        ip += 2
+    elif instruction == 4:
+        output_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            output_val = program[output_val]
+        elif parameter_mode[0] == 2:
+            output_val = program[output_val + rb]
+        outputs.append(output_val)
+
+        if len(outputs) == 3:
+            x, y, tileId = outputs
+            if x == -1 and y == 0:
+                score = tileId
+            else:
+                world[x][y] = tileId
+
+                if tileId == 3:
+                    paddle_x, paddle_y = x, y
+                elif tileId == 4:
+                    ball_x, ball_y = x, y
+
+            outputs = []
+        ip += 2
+    elif instruction == 5:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        if first_val != 0:
+            ip = second_val
+        else:
+            ip += 3
+    elif instruction == 6:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        if first_val == 0:
+            ip = second_val
+        else:
+            ip += 3
+    elif instruction == 7:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        if first_val < second_val:
+            program[output_address] = 1
+        else:
+            program[output_address] = 0
+        ip += 4
+    elif instruction == 8:
+        first_val = program[ip+1]
+        if parameter_mode[0] == 0:
+            first_val = program[first_val]
+        elif parameter_mode[0] == 2:
+            first_val = program[first_val + rb]
+
+        second_val = program[ip+2]
+        if parameter_mode[1] == 0:
+            second_val = program[second_val]
+        elif parameter_mode[1] == 2:
+            second_val = program[second_val + rb]
+
+        output_address = program[ip+3]
+        if parameter_mode[2] == 2:
+            output_address += rb
+
+        if first_val == second_val:
+            program[output_address] = 1
+        else:
+            program[output_address] = 0
+        ip += 4
+    elif instruction == 9:
+        val = program[ip+1]
+        if parameter_mode[0] == 0:
+            val = program[val]
+        elif parameter_mode[0] == 2:
+            val = program[val + rb]
+        rb += val
+        ip += 2
+
+print(score)
